38ef72f81e9894e8cfad0b5af3182708494472b3
Ben Kunkle <ben.kunkle@gmail.com>
2023-05-10 17:06:48 -0700

    function to get all majors,catologs,concentrations from polyflows

--- polyflows.js
+++ polyflows.js
@@ -1,17 +1,17 @@
-
 const DOMAIN = "https://polyflowbuilder.duncanapple.io";
 
 const getSCookie = async () => {
-    return fetch(DOMAIN)
-        .then((res) => res.headers.get("set-cookie").split(";")[0])
-}
+    return fetch(DOMAIN).then(
+        (res) => res.headers.get("set-cookie").split(";")[0]
+    );
+};
 
 const createHeaders = (s) => {
     const headers = new Headers();
     // encode cookies properly
     headers.append("Cookie", s);
     return headers;
-}
+};
 
 const login = async (s) => {
     require("dotenv").config();
@@ -29,10 +29,10 @@ const login = async (s) => {
         method: "POST",
         body: formData,
         headers,
-    }).then(res => console.assert(res.status === 200, "Login failed"));
-}
+    }).then((res) => console.assert(res.status === 200, "Login failed"));
+};
 
-const getFlow = async (catologYear,major) => {
+const getFlow = async (catologYear, major) => {
     const s = await getSCookie();
 
     await login(s);
@@ -50,15 +50,29 @@ const getFlow = async (catologYear,major) => {
             options: { fgoRemoveGE: false },
         }),
         redirect: "follow",
-    })
+    });
     // assert defaultFLow response content type is json
     if (defaultFlow.headers.get("content-type").includes("html")) {
-        console.log("sent back html")
-    } else if (defaultFlow.headers.get("content-type").includes("json")){
-        console.log(await defaultFlow.json())
+        console.log("sent back html");
+    } else if (defaultFlow.headers.get("content-type").includes("json")) {
+        console.log(await defaultFlow.json());
     }
-}
+};
+
+const fetchAllOptions = async () => {
+    const s = await getSCookie();
+    const headers = createHeaders(s);
+    await login(s);
+
+    return fetch(DOMAIN + "/api/data/getAvailableFlowchartMetadata", {
+        method: "GET",
+        headers,
+    });
+};
 
 (async () => {
-    await getFlow("2021-2022", "52CSCBSU");
+    // await getFlow("2021-2022", "52CSCBSU");
+    await fetchAllOptions()
+        .then((res) => res.json())
+        .then(console.log);
 })();
