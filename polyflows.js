c4e4f4f753a86b01386cab61ef009e200fc6782f
Ben Kunkle <ben.kunkle@gmail.com>
2023-05-10 17:00:00 -0700

    break polyflows into sub functions

--- polyflows.js
+++ polyflows.js
@@ -1,41 +1,51 @@
-require("dotenv").config();
 
 const DOMAIN = "https://polyflowbuilder.duncanapple.io";
 
-(async () => {
-    const s = await fetch(DOMAIN)
+const getSCookie = async () => {
+    return fetch(DOMAIN)
         .then((res) => res.headers.get("set-cookie").split(";")[0])
+}
 
+const createHeaders = (s) => {
+    const headers = new Headers();
+    // encode cookies properly
+    headers.append("Cookie", s);
+    return headers;
+}
+
+const login = async (s) => {
+    require("dotenv").config();
     const email = process.env.POLYFLOWBUILDER_EMAIL;
     const password = process.env.POLYFLOWBUILDER_PWORD;
 
     const formData = new URLSearchParams();
     formData.append("email", email);
     formData.append("password", password);
-    const login = { email, password };
-    console.log(login);
 
-    const headers = new Headers();
-    // encode cookies properly
-    headers.append("Cookie", s);
+    const headers = createHeaders(s);
     headers.set("Content-Type", "application/x-www-form-urlencoded");
 
-    await fetch(DOMAIN + "/login", {
+    return fetch(DOMAIN + "/login", {
         method: "POST",
         body: formData,
         headers,
     }).then(res => console.assert(res.status === 200, "Login failed"));
+}
 
+const getFlow = async (catologYear,major) => {
+    const s = await getSCookie();
+
+    await login(s);
+
+    const headers = createHeaders(s);
     headers.set("Content-Type", "application/json");
-    // headers.set("Cookie", s);
-    console.log(headers);
 
     const defaultFlow = await fetch(DOMAIN + "/api/util/getDefaultFlow", {
         method: "POST",
         headers,
         body: JSON.stringify({
-            flowCatalogYear: "2021-2022",
-            flowMajor: "52CSCBSU",
+            flowCatalogYear: catologYear,
+            flowMajor: major,
             flowConcentration: "",
             options: { fgoRemoveGE: false },
         }),
@@ -47,4 +57,8 @@ const DOMAIN = "https://polyflowbuilder.duncanapple.io";
     } else if (defaultFlow.headers.get("content-type").includes("json")){
         console.log(await defaultFlow.json())
     }
+}
+
+(async () => {
+    await getFlow("2021-2022", "52CSCBSU");
 })();
