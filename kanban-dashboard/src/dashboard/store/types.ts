21f0ccd6c40e5ce8f61ae8a59219a9a46cf806ab
Ben Kunkle <ben.kunkle@gmail.com>
2023-05-13 12:41:06 -0700

    move quarters data to backend and use trpc request

--- kanban-dashboard/src/dashboard/store/types.ts
+++ kanban-dashboard/src/dashboard/store/types.ts
@@ -1,50 +1,50 @@
-import { State as NormalizedState } from 'normalized-reducer';
+import { UUID } from "crypto";
+import { State as NormalizedState } from "normalized-reducer";
 
 export interface Flowchart {
-  meta: FlowchartMeta,
-  data: FlowchartData,
+  meta: FlowchartMeta;
 }
 
 export interface FlowchartMeta {
-  id,
-  title: string,
-  description: string,
+  id: UUID;
+  title: string;
+  description: string;
 }
 
 export interface FlowchartData extends NormalizedState {
   entities: {
-    user: Record<string, User>,
-    task: Record<string, Course>,
-    status: Record<string, Status>,
-    tag: Record<string, Tag>,
-    comment: Record<string, Comment>,
-  },
+    user: Record<string, User>;
+    task: Record<string, Course>;
+    status: Record<string, Status>;
+    tag: Record<string, Tag>;
+    comment: Record<string, Comment>;
+  };
   ids: {
-    user: string[],
-    task: string[],
-    status: string[],
-    tag: string[],
-    comment: string[],
-  }
+    user: string[];
+    task: string[];
+    status: string[];
+    tag: string[];
+    comment: string[];
+  };
 }
 
 export interface User {
-  id: string,
-  username: string,
-  createdCourseIds?: string[],
-  assignedCourseIds?: string[],
-  commentIds?: string[],
+  id: string;
+  username: string;
+  createdCourseIds?: string[];
+  assignedCourseIds?: string[];
+  commentIds?: string[];
 }
 
 export interface Course {
-  id: string,
-  title: string,
-  description: string,
-  statusId: string,
-  creatorId: string,
-  assigneeId?: string,
-  tagIds?: string[],
-  rootCommentIds?: string[],
+  id: string;
+  title: string;
+  description: string;
+  statusId: string;
+  creatorId: string;
+  assigneeId?: string;
+  tagIds?: string[];
+  rootCommentIds?: string[];
 }
 
 export enum CourseType {
@@ -52,24 +52,24 @@ export enum CourseType {
 }
 
 export interface Status {
-  id: string,
-  title: string,
-  taskIds?: string[]
+  id: string;
+  title: string;
+  taskIds?: string[];
 }
 
 export interface Tag {
-  id: string,
-  value: string
-  taskIds?: string[]
+  id: string;
+  value: string;
+  taskIds?: string[];
 }
 
 export interface Comment {
-  id: string,
-  value: string,
-  creatorId: string,
-  taskId?: string,
-  parentCommentId?: string,
-  childCommentIds?: string[],
+  id: string;
+  value: string;
+  creatorId: string;
+  taskId?: string;
+  parentCommentId?: string;
+  childCommentIds?: string[];
 }
 
-export type UpdateFlowchartData = (projectData: FlowchartData) => Promise<void>
+export type UpdateFlowchartData = (projectData: FlowchartData) => Promise<void>;
