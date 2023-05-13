75cf74addcb79575df899ab84d289d52d148706c
Ben Kunkle <ben.kunkle@gmail.com>
2023-05-13 13:41:58 -0700

    move random course gen to server

--- kanban-dashboard/src/dashboard/Flowchart.tsx
+++ kanban-dashboard/src/dashboard/Flowchart.tsx
@@ -1,21 +1,23 @@
-import React, { useState } from 'react';
-import Paper from '@material-ui/core/Paper';
-import Dialog from '@material-ui/core/Dialog';
+import React, { useCallback, useEffect, useState } from "react";
+import Paper from "@material-ui/core/Paper";
+import Dialog from "@material-ui/core/Dialog";
 import {
   DragDropContext,
   DropResult,
   Droppable,
   DroppableProvided,
-} from 'react-beautiful-dnd';
-import AddIcon from '@material-ui/icons/Add';
-import { hooks, emptyArray } from './store';
-import { useBoardStyles } from './styles';
-import StatusLane from './StatusLane';
-import { Fab } from '@material-ui/core';
-import CourseEditorForm from './CourseEditorForm';
-import { useCurrentUserId } from './CurrentUser';
-import {handleCloseModal} from "../helpers/shared"
+} from "react-beautiful-dnd";
+import AddIcon from "@material-ui/icons/Add";
+import { hooks, emptyArray } from "./store";
+import { useBoardStyles } from "./styles";
+import StatusLane from "./StatusLane";
+import { Fab } from "@material-ui/core";
+import CourseEditorForm from "./CourseEditorForm";
+import { useCurrentUserId } from "./CurrentUser";
+import { handleCloseModal } from "../helpers/shared";
 
+import { api } from "~/utils/api";
+import { UUID } from "crypto";
 
 export default function Flowchart() {
   const currentUserId = useCurrentUserId();
@@ -24,164 +26,91 @@ export default function Flowchart() {
   const moveStatus = hooks.useMoveStatus();
   const moveCourse = hooks.useMoveStatusCourse();
 
-
   const [isCourseFormOpen, setIsCourseFormOpen] = useState(false);
   const openCourseForm = () => setIsCourseFormOpen(true);
   const closeCourseForm = () => setIsCourseFormOpen(false);
 
   const classNames = useBoardStyles();
 
-
-  const handleDragEnd = ({ type, source, destination, draggableId }: DropResult) => {
+  const handleDragEnd = ({
+    type,
+    source,
+    destination,
+    draggableId,
+  }: DropResult) => {
     if (source && destination) {
-      if (type === 'statusLane' && moveStatus) {
+      if (type === "statusLane" && moveStatus) {
         moveStatus(source.index, destination.index);
       }
 
-      if (type === 'taskCard' && moveCourse) {
+      if (type === "taskCard" && moveCourse) {
         moveCourse(
           draggableId,
           source.droppableId,
           source.index,
           destination.droppableId,
           destination.index
-        )
+        );
       }
     }
   };
+  const courseQuery = api.poly.courses.useQuery();
+  useEffect(() => {
+    if (courseQuery.isSuccess && courseQuery.data) {
+      courseQuery.data.forEach((course) => {
+        createCourse({
+          title: course.classcode,
+          statusId: course.status,
+          creatorId: currentUserId,
+          description: course.classname,
+        });
+      });
+    }
+  }, [courseQuery.isLoading]);
 
-  const handleSubmitNewCourse = (title: string, desc: string) => {
+  const handleSubmitNewCourse = (_title: string, _desc: string) => {
     if (createCourse && currentUserId) {
-      const statuses = [
-        "c41ba2a3-5068-4a8f-b8b0-568ca295ef56",
-        "49237786-3411-4ab5-974e-3b0078643bab",
-        "c40bfef2-31c2-4228-a8cc-22b52974fbc7",
-        "c40bfef2-31c2-4228-a8cc-22b52974fbc7",
-        "c41ba2a3-5068-4a8f-b8b5-568ca295ef56",
-        "c40bfef2-31c2-4228-a8c6-22b52974fbc7",
-        "c41ba2a3-5068-4a8f-b8b7-568ca295ef56",
-        "c41ba2a3-5068-4a8f-b8b6-568ca295ef56",
-        "49237786-3411-4ab5-9746-3b0078643bab",
-        "c40bfef2-31c2-4228-a8c6-22b52974fbc7",
-        "c41ba2a3-5068-4a8f-b8b7-568ca295ef56",
-        "49237786-3411-4ab5-9747-3b0078643bab",
-        "c40bfef2-31c2-4228-a8c7-22b52974fbc7",
-      ]
-
-      const courses = [
-        {
-          classcode: 'CSC 101',
-          classname: 'Intro to Computer Science',
-        },
-        {
-          classcode: 'CSC 102',
-          classname: 'Data Structures and Algorithms',
-        },
-        {
-          classcode: 'CSC 202',
-          classname: 'Discrete Structures',
-        },
-        {
-          classcode: 'CSC 225',
-          classname: 'Introduction to Computer Organization',
-        },
-        {
-          classcode: 'CSC 248',
-          classname: 'Introduction to Database Systems',
-        },
-        {
-          classcode: 'CSC 307',
-          classname: 'Systems Programming',
-        },
-        {
-          classcode: 'CSC 357',
-          classname: 'Design & Analysis of Algorithms',
-        },
-        {
-          classcode: 'CSC 453',
-          classname: 'Operating Systems',
-        },
-        {
-          classcode: 'CSC 491',
-          classname: 'Senior Project Lab I',
-        },
-        {
-          classcode: 'CSC 492',
-          classname: 'Senior Project Lab II',
-        },
-        {
-          classcode: 'CSC 497',
-          classname: 'Research Senior Project I',
-        },
-        {
-          classcode: 'CPE 102',
-          classname: 'Introduction to Computer Science II',
-        },
-        {
-          classcode: 'CPE 103',
-          classname: 'Object-Oriented Design',
-        },
-        {
-          classcode: 'CPE 357',
-          classname: 'Introduction to Software Engineering',
-        },
-        {
-          classcode: 'MATH 141',
-          classname: 'Calculus I',
-        },
-        {
-          classcode: 'MATH 142',
-          classname: 'Calculus II',
-        },
-        {
-          classcode: 'MATH 206',
-          classname: 'Statistical Methods for Engineers',
-        },
-        {
-          classcode: 'MATH 244',
-          classname: 'Applied Linear Models',
-        },
-      ];
-      
-
-      [...statuses].forEach(status => {
-        const course1 = courses[Math.round(Math.random() * courses.length)]
-        const course2 = courses[Math.round(Math.random() * courses.length)]
-        if(course1) {
-          createCourse({ title: course1.classcode, statusId: status, creatorId: currentUserId, description: course1.classname, });
-        }
-        if(course2) {
-          createCourse({ title: course2.classcode, statusId: status, creatorId: currentUserId, description: course2.classname, });
-        }
-      })
-      //createCourse({ title, statusId: status, creatorId: currentUserId, description: desc, });
-      
+      // pass
     }
     closeCourseForm();
   };
 
   return (
     <div className={classNames.board}>
-      {createCourse && (
-          <Dialog open={isCourseFormOpen} onClose={(event, reason) => (handleCloseModal(event, reason, closeCourseForm))}>
-            <Paper className={classNames.dialog}>
-              <CourseEditorForm onSubmit={handleSubmitNewCourse} onCancel={closeCourseForm}/>
-            </Paper>
-          </Dialog>
-        )}
+      <Dialog
+        open={isCourseFormOpen}
+        onClose={(event, reason) =>
+          handleCloseModal(event, reason, closeCourseForm)
+        }
+      >
+        <Paper className={classNames.dialog}>
+          <CourseEditorForm
+            onSubmit={handleSubmitNewCourse}
+            onCancel={closeCourseForm}
+          />
+        </Paper>
+      </Dialog>
       <DragDropContext onDragEnd={handleDragEnd}>
-        <Droppable type="statusLane" droppableId="projectBoard" direction="horizontal">
+        <Droppable
+          type="statusLane"
+          droppableId="projectBoard"
+          direction="horizontal"
+        >
           {(provided: DroppableProvided) => {
             return (
-              <div ref={provided.innerRef} {...provided.droppableProps} className={classNames.lanes}>
+              <div
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+                className={classNames.lanes}
+              >
                 {(statusIds || emptyArray).map((statusId, index) => (
-                   <div className={classNames.laneContainer} key={index}>
-                   <StatusLane id={statusId}  />
-                 </div>
+                  <div className={classNames.laneContainer} key={index}>
+                    <StatusLane id={statusId} />
+                  </div>
                 ))}
                 {provided.placeholder}
               </div>
-            )
+            );
           }}
         </Droppable>
       </DragDropContext>
