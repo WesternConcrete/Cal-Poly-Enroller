2179845bf17e08a958e69fcfd80b3a7a83f231b1
Wes Convery <2wconvery@gmail.com>
2023-06-01 21:30:22 -0700

    auth page

---
+++ kanban-dashboard/src/pages/auth.tsx
@@ -0,0 +1,17 @@
+export default function () {
+    return (
+        <section className="container flex flex-col items-center justify-center gap-6 pb-8 pt-6 md:py-10">
+        <div className="flex flex-col items-center gap-2 max-w-[980px] text-center">
+          <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+            Welcome to the Cal Poly Flowchart Module
+          </h1>
+          <p className="max-w-[700px] text-lg text-muted-foreground">
+            The Cal Poly Flowchart Module is a personalized tool, integrated into the Cal Poly enrollment software, that allows students to track their major progress, plan future classes, and easily enroll in up-to-date courses, enhancing efficiency and effectiveness in academic planning.
+          </p>
+        </div>
+        <div className="flex justify-center gap-4">
+         
+        </div>
+      </section>
+    )
+}
