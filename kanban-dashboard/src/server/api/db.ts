1f8e203c22df4d1cf323343a9fe8f89bf7d80d39
Ben Kunkle <ben.kunkle@gmail.com>
2023-06-08 10:51:12 -0700

    working course requirement creation

--- kanban-dashboard/src/server/api/db.ts
+++ kanban-dashboard/src/server/api/db.ts
@@ -11,10 +11,10 @@ import {
   CourseCode,
 } from "../../scraping/catalog";
 
-const CREATE_DEGREES = true;
-const CREATE_COURSES = true;
+const CREATE_DEGREES = false;
+const CREATE_COURSES = false;
 const CREATE_GE_REQUIREMENTS = false;
-const CREATE_COURSE_REQUIREMENTS = false;
+const CREATE_COURSE_REQUIREMENTS = true;
 const skipDuplicates = true;
 const createCourses = async (prisma: PrismaClient) => {
   const subjects = await scrapeSubjects();
@@ -71,6 +71,84 @@ const createDegrees = async (prisma: PrismaClient) => {
   );
 };
 
+const createCourseRequirements = async (
+  prisma: PrismaClient,
+  sections: DegreeRequirementSection[],
+  degreeId: string
+) => {
+  for (const section of sections) {
+    if (section.kind === "ge") continue;
+    if (section.courses.length === 0) continue;
+
+    const createGroup = async (group: CourseRequirement): Promise<number> => {
+      const courses: CourseCode[] = [];
+      const childGroups = [];
+      let kind;
+      if (group.kind === "course") {
+        throw new Error("cannot create group from only course");
+      }
+      for (let subGroup of group.courses) {
+        if (subGroup.kind === "course") {
+          courses.push(subGroup.course);
+          continue;
+        }
+        let subGroupId = await createGroup(subGroup);
+        childGroups.push(subGroupId);
+      }
+
+      let courseKindInfo = null;
+      if (section.kind === "elective") {
+        courseKindInfo = section.electiveKind;
+      } else if (section.kind === "support") {
+        courseKindInfo = section.supportKind;
+      }
+
+      const createdGroup = await prisma.courseRequirementGroup.create({
+        data: {
+          groupKind: group.kind as "or" | "and",
+          coursesKind: section.kind,
+          courseKindInfo,
+          courses: {
+            createMany: {
+              data: courses.map((c: CourseCode) => ({
+                courseCode: c,
+                kind: section.kind,
+              })),
+            skipDuplicates,
+            },
+          },
+          ...(childGroups.length > 0 && {
+            childGroups: {
+              connect: childGroups.map((id) => ({ id })),
+            },
+          }),
+        },
+        select: {
+          id: true,
+        },
+      }).catch((e) => { console.error("failed to create group:",group); throw e});
+      return createdGroup.id;
+    };
+
+    let groupKind: "or" | "and" = section.kind === "elective" ? "or" : "and";
+    let rootGroupId = await createGroup({
+      kind: groupKind,
+      courses: section.courses,
+      // FIXME: scrape degree units
+      units: 0,
+    });
+
+    await prisma.degree.update({
+      where: { id: degreeId },
+      data: {
+        requirements: {
+          connect: [{ id: rootGroupId }],
+        },
+      },
+    });
+  }
+};
+
 export const updateCatalogDataInDB = async (prisma: PrismaClient) => {
   if (CREATE_COURSES) await createCourses(prisma);
 
@@ -86,6 +164,13 @@ export const updateCatalogDataInDB = async (prisma: PrismaClient) => {
         kind: true,
       },
     });
+  }
+  if (CREATE_GE_REQUIREMENTS || CREATE_COURSE_REQUIREMENTS) {
+    for (let degree of degrees) {
+      const requirements = await scrapeDegreeRequirements(degree);
+      if (CREATE_COURSE_REQUIREMENTS)
+        await createCourseRequirements(prisma, requirements.courses, degree.id);
+    }
   }
   // FIXME: use CourseCodeSchema for ALL course codes to catch weird edge cases and prevent the "not found" errors when connecting
   // FIXME: implement retry logic as connection is very unstable
