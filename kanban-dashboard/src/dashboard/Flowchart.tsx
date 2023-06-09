50613382b44035a2848d3fac45b94b324d6e5172
Wes Convery <2wconvery@gmail.com>
2023-06-08 17:37:35 -0700

    overflow

--- kanban-dashboard/src/dashboard/Flowchart.tsx
+++ kanban-dashboard/src/dashboard/Flowchart.tsx
@@ -9,6 +9,7 @@ import {
 } from "~/dashboard/state";
 import { api } from "~/utils/api";
 import CollapsedQuarter from "./CollapsedQuarter";
+import { Trash } from "lucide-react";
 
 export default function Flowchart() {
   const { startYear, selectedRequirements, setSelectedRequirements } =
@@ -74,16 +75,16 @@ export default function Flowchart() {
     sliceStart: number,
     sliceEnd: number
   ) => (
-    <div className="flex flex-col h-full">
-      <div className="bg-black text-white text-center p-2 font-bold">
+    <div className="flex flex-col h-full overflow-hidden">
+      <div className="text-center p-1 font-bold border-x">
         {title}
       </div>
-      <div className="flex justify-center h-full">
+      <div className="flex justify-center h-full overflow-y-hidden">
         {quartersQuery.data
           ? quartersQuery.data.slice(sliceStart, sliceEnd).map((quarter) => (
               <div
-                className="flex-shrink-0 w-[120px] overflow-x-hidden overflow-y-scroll flex flex-col items-center relative border-[1px] border-solid border-[#e1e4e8] mx-0"
-                key={quarter.id}
+              className="flex-basis-[100%] flex-grow-0 overflow-x-hidden overflow-y-scroll w-full flex w-[140px] flex-col relative border-[1px] border-solid border-[#e1e4e8]"
+              key={quarter.id}
               >
                 <Quarter quarter={quarter} />
               </div>
@@ -93,9 +94,11 @@ export default function Flowchart() {
     </div>
   );
 
+
   return (
+    
     <div className="h-full flex overflow-x-auto">
-      <DragDropContext
+      {quartersQuery.data && <DragDropContext
         onDragEnd={handleDragEnd}
         onDragStart={handleDragStart}
         onDragUpdate={handleDragUpdate}
@@ -111,18 +114,20 @@ export default function Flowchart() {
           }}
         >
           <div className="flex-grow-1 flex overflow-x-scroll scroll-behavior-smooth w-full">
-            <div className="flex gap-1">
-              <div className="flex-basis-[100%] flex-grow-0 overflow-x-hidden overflow-y-scroll w-full flex min-w-[140px] flex-col relative border-[1px] border-solid border-[#e1e4e8]">
+            <div className="flex">
+
+              <div   className="flex-basis-[100%] flex-grow-0 overflow-x-hidden overflow-y-scroll w-full flex w-[220px] flex-col relative border-[1px] border-solid border-[#e1e4e8] bt-none">
                 <CollapsedQuarter quarter={collapsedQuarterData} />
               </div>
-              {renderQuarterGroup("Freshmen", 0, 3)}
+              {renderQuarterGroup("Freshman", 0, 3)}
               {renderQuarterGroup("Sophomore", 3, 6)}
               {renderQuarterGroup("Junior", 6, 9)}
               {renderQuarterGroup("Senior", 9, Infinity)}
             </div>
           </div>
         </DraggingState.Provider>
-      </DragDropContext>
+      </DragDropContext>}
+      
     </div>
   );
 }
